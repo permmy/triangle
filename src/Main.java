2cfa99d26168cc3498ea312cedc3f258dfc04b64
permmy <gathangaperminus@gmail.com>
2018-06-21 12:38:34 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    // Function to print first
+    // n lines of Pascal's Triangle
+    static void printPascal(int n)
+    {
+
+        // Iterate through every line
+        // and print entries in it
+        for (int line = 0; line < n; line++)
+        {
+            // Every line has number of
+            // integers equal to line number
+            for (int i = 0; i <= line; i++)
+                System.out.print(binomialCoeff
+                        (line, i)+" ");
+
+            System.out.println();
+        }
+    }
+
+    // Link for details of this function
+    // https://www.geeksforgeeks.org/archives/25621
+    static int binomialCoeff(int n, int k)
+    {
+        int res = 1;
+
+        if (k > n - k)
+            k = n - k;
+
+        for (int i = 0; i < k; ++i)
+        {
+            res *= (n - i);
+            res /= (i + 1);
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int n = 7;
+        printPascal(n);
+    }
+}
